df25379126229ad737d72eae912280885b25387e
KotypeyPyEdition <kotypey@gmail.com>
2020-06-19 02:00:59 +0300

    still so bad frontend

--- src/store/index.js
+++ src/store/index.js
@@ -24,13 +24,7 @@ export default new Vuex.Store({
   actions: {
 
     async login({commit}, options){
-      let response = await Vue.axios.post('/frontend/api/v1/auth/login', JSON.stringify(
-          {
-
-            login: options.login,
-            password: options.password,
-            ip: this.ip
-          })).then(res => {
+      let response = await Vue.axios.post('/frontend/api/v1/auth/login', {login: options.login, password: options.password, ip: this.ip}).then(res => {
         if(res.data.token) commit('setToken', res.data.token);
         else {
           alert('No token, maybe you made a typo?')
