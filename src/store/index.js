cb5b2283487d7c13edb3fcda223a4559eba0fe1d
KotypeyPyEdition <kotypey@gmail.com>
2020-06-19 02:07:29 +0300

    still so bad frontend

--- src/store/index.js
+++ src/store/index.js
@@ -24,13 +24,20 @@ export default new Vuex.Store({
   actions: {
 
     async login({commit}, options){
-      let response = await Vue.axios.post('/frontend/api/v1/auth/login', JSON.stringify({login: options.login, password: options.password, ip: this.ip}), {
-        data: {login: options.login, password: options.password, ip: this.ip}
-      }).then(res => {
-        if(res.data.token) commit('setToken', res.data.token);
-        else {
-          alert('No token, maybe you made a typo?')
+
+      let response = await fetch('/frontend/api/v1/auth/login', {
+        body: JSON.stringify({login: options.login, password: options.password, ip: this.ip}),
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
         }
+      }).then(resp => {
+        resp.json().then(async res => {
+          if(res.token) commit('setToken', res.token);
+          else {
+            alert('No token, maybe you made a typo?')
+          }
+        })
       }).catch(e => {
         alert('Invalid login data or captcha ...')
       })
